904de58bde0bacc7d75918b9364c2642884413b6
plotski <plotski@example.org>
2020-10-28 12:35:02 +0100

    Submission: Derive "success" from "error" and "filename" from "filepath"

--- pyimgbox/_submission.py
+++ pyimgbox/_submission.py
@@ -1,25 +1,32 @@
+import os
+
+
 class Submission(dict):
     """
     Dictionary with the following keys:
 
     success: True or False
     error: Error message or None
-    name: File name or None
-    path: File path or None
+    filename: File name or None
+    filepath: File path or None
     image_url: URL to image or None
+    thumbnail_url: URL to thumbnail or None
     web_url: URL to image's web page or None
     gallery_url: URL to web page of thumbnails or None
     edit_url: URL to manage gallery or None
 
+    "success" is derived from "error".
+    "filename" is derived from "filepath".
+
     All keys are also available as attributes for convenience.
     """
 
-    def __init__(self, *, success, **kwargs):
+    def __init__(self, **kwargs):
         values = {
-            'success': bool(success),
+            'success': None,
             'error': None,
-            'filename': None,
             'filepath': None,
+            'filename': None,
             'image_url': None,
             'thumbnail_url': None,
             'web_url': None,
@@ -28,14 +35,18 @@ class Submission(dict):
         }
         for k in kwargs:
             assert k in values, f'Unknown key: {k!r}'
-        if not success:
-            assert 'error' in kwargs, "Missing key: 'error'"
-        else:
-            assert 'error' not in kwargs, "Conflicting keys: 'error', 'success'"
-            for k in ('filename', 'filepath', 'image_url', 'thumbnail_url',
+
+        if not kwargs.get('error'):
+            for k in ('filepath', 'image_url', 'thumbnail_url',
                       'web_url', 'gallery_url', 'edit_url'):
                 assert k in kwargs, f'Missing key: {k!r}'
+
         values.update(kwargs)
+
+        if values.get('filepath'):
+            values['filename'] = os.path.basename(values['filepath'])
+        values['success'] = not bool(values.get('error'))
+
         super().__init__(values)
 
     def __getattr__(self, name):
--- tests/test_submission.py
+++ tests/test_submission.py
@@ -3,59 +3,29 @@ import pytest
 from pyimgbox import Submission
 
 
-def test_Submission_needs_success_key():
-    with pytest.raises(TypeError, match=r"required keyword-only argument: 'success'$"):
-        Submission(x='y')
-
 def test_Submission_gets_unknown_key():
     with pytest.raises(AssertionError, match=r"^Unknown key: 'x'$"):
-        Submission(success=False, x='y')
+        Submission(x='y')
 
-def test_Submission_gets_failure_and_no_error():
-    with pytest.raises(AssertionError, match=r"^Missing key: 'error'$"):
-        Submission(success=False, filename='foo')
+@pytest.mark.parametrize(
+    argnames='key',
+    argvalues=('filepath', 'image_url', 'thumbnail_url', 'web_url', 'gallery_url', 'edit_url'),
+)
+def test_Submission_required_argument_on_success(key):
+    with pytest.raises(AssertionError, match=rf"^Missing key: '{key}'$"):
+        kwargs = {
+            'filepath': 'path/to/Foo.jpg',
+            'image_url': 'https://foo.bar/asdf.jpg',
+            'thumbnail_url': 'https://foo.bar/asdf_t.jpg',
+            'web_url': 'https://foo.bar/asdf',
+            'gallery_url': 'https://foo.bar/asdf/gallery',
+            'edit_url': 'https://foo.bar/asdf/edit',
+        }
+        del kwargs[key]
+        Submission(**kwargs)
 
-def test_Submission_gets_conflicting_success_and_error():
-    with pytest.raises(AssertionError, match=r"^Conflicting keys: 'error', 'success'$"):
-        Submission(success=True, error='Huh?')
-
-def test_Submission_gets_success_and_not_all_expected_keys():
-    expected_keys = {'filename': 'Foo.jpg',
-                     'filepath': 'path/to/Foo.jpg',
-                     'image_url': 'https://foo.bar/asdf.jpg',
-                     'thumbnail_url': 'https://foo.bar/asdf_t.jpg',
-                     'web_url': 'https://foo.bar/asdf',
-                     'gallery_url': 'https://foo.bar/fdsa',
-                     'edit_url': 'https://foo.bar/fdsa/edit'}
-    for k in expected_keys:
-        with pytest.raises(AssertionError, match=rf"^Missing key: '{k}'$"):
-            keys = expected_keys.copy()
-            del keys[k]
-            Submission(success=True, **keys)
-
-def test_Submission_gets_valid_success_arguments():
+def test_Submission_no_required_arguments_on_error():
     assert Submission(
-        success=True,
-        filename='Foo.jpg',
-        filepath='path/to/Foo.jpg',
-        image_url='https://foo.bar/asdf.jpg',
-        thumbnail_url='https://foo.bar/asdf_t.jpg',
-        web_url='https://foo.bar/asdf',
-        gallery_url='https://foo.bar/fdsa',
-        edit_url='https://foo.bar/fdsa/edit'
-    ) == {'success': True,
-          'error': None,
-          'filename': 'Foo.jpg',
-          'filepath': 'path/to/Foo.jpg',
-          'image_url': 'https://foo.bar/asdf.jpg',
-          'thumbnail_url': 'https://foo.bar/asdf_t.jpg',
-          'web_url': 'https://foo.bar/asdf',
-          'gallery_url': 'https://foo.bar/fdsa',
-          'edit_url': 'https://foo.bar/fdsa/edit'}
-
-def test_Submission_gets_valid_error_arguments():
-    assert Submission(
-        success=False,
         error='Trouble is afoot!',
     ) == {'success': False,
           'error': 'Trouble is afoot!',
@@ -66,3 +36,23 @@ def test_Submission_gets_valid_error_arguments():
           'web_url': None,
           'gallery_url': None,
           'edit_url': None}
+
+def test_Submission_gets_valid_success_arguments():
+    assert Submission(
+        filepath='path/to/Foo.jpg',
+        image_url='https://foo.bar/asdf.jpg',
+        thumbnail_url='https://foo.bar/asdf_t.jpg',
+        web_url='https://foo.bar/asdf',
+        gallery_url='https://foo.bar/fdsa',
+        edit_url='https://foo.bar/fdsa/edit'
+    ) == {
+        'success': True,
+        'error': None,
+        'filename': 'Foo.jpg',
+        'filepath': 'path/to/Foo.jpg',
+        'image_url': 'https://foo.bar/asdf.jpg',
+        'thumbnail_url': 'https://foo.bar/asdf_t.jpg',
+        'web_url': 'https://foo.bar/asdf',
+        'gallery_url': 'https://foo.bar/fdsa',
+        'edit_url': 'https://foo.bar/fdsa/edit',
+    }
