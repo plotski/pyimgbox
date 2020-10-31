8473f1c963d3f65755d86c1d1115593fb71ca138
plotski <plotski@example.org>
2020-10-31 12:39:45 +0100

    Remove old variable __command_name__

--- pyimgbox/__init__.py
+++ pyimgbox/__init__.py
@@ -1,7 +1,6 @@
 __version__ = '1.0.0'
 __author__ = 'plotski'
 __author_email__ = 'plotski@example.org'
-__command_name__ = 'imgbox'
 
 from ._const import MAX_FILE_SIZE  # noqa: F401
 from ._gallery import Gallery  # noqa: F401
